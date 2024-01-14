7d7999d16d242aeddfd19b58303a9032bd05084c
Jorge Fernandez <jorge.fernandez@uala.com.ar>
2024-01-14 11:49:39 -0300

    fix(go): order

--- SaveOperations/pkg/models/request.go
+++ SaveOperations/pkg/models/request.go
@@ -1,14 +1,14 @@
 package models
 
 type Operation struct {
-	Amount             float64           `json:"amount"`
+	PaymentInstruction map[string]string `json:"paymentInstruction"`
 	CreditoAccountCode string            `json:"creditorAccountCode"`
 	CreditorEntityCode string            `json:"creditorEntityCode"`
 	DebtorAccountCode  string            `json:"debtorAccountCode"`
 	DebtorEntityCode   string            `json:"debtorEntityCode"`
 	InternalNumber     string            `json:"internalNumber,omitempty"`
 	OperationCode      string            `json:"operationCode"`
-	PaymentInstruction map[string]string `json:"paymentInstruction"`
+	Amount             float64           `json:"amount"`
 }
 
 type Request struct {
